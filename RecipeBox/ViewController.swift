55b29cac11deafa7535c0514fbaad6370b9bc7a0
tataaval <94060509+tataaval@users.noreply.github.com>
2024-10-11 20:13:51 +0400

    Initial Commit

---
+++ RecipeBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeBox
+//
+//  Created by Tatarella on 11.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
